63476751222f6e18e0a75836a75075e7da6353f9
Yigit Akture <220706301@st.maltepe.edu.tr>
2025-05-10 20:20:08 +0300

    remove unnecessary assignment

--- edupilot_api/EduPilot.Api/Data/Models/Question.cs
+++ edupilot_api/EduPilot.Api/Data/Models/Question.cs
@@ -6,7 +6,7 @@
         public Guid QuizId { get; set; }
         public string QuestionContent { get; set; }
         public string? QuestionImage { get; set; }
-        public bool IsActive { get; set; } = true;
+        public bool IsActive { get; set; }
         public List<Choice> Choices { get; set; }
         public Quiz Quiz { get; set; }
     }
